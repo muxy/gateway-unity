f9b2cfd1de21e29a47a6bbdc10941dedf36093e2
Troy <tshanks@muxy.io>
2023-05-10 11:20:24 -0400

    Current Prefab work

---
+++ Prefabs/MuxyGatewayNotificationHistory.cs
@@ -0,0 +1,64 @@
+using TMPro;
+using UnityEngine;
+
+public class GatewayNotificationHistory : MonoBehaviour
+{
+    public float ClearTime = 5;
+    public GameObject HistoryGroup;
+
+
+    private float ClearTimer = 5;
+
+    public void Update()
+    {
+        ClearTimer -= Time.deltaTime;
+        if (ClearTimer <= 0)
+        {
+            ClearEntries();
+        }
+    }
+
+    public void ShiftEntries()
+    {
+        for (int i = HistoryGroup.transform.childCount; i-- > 0;)
+        {
+            TMP_Text TmpTextRight = HistoryGroup.transform.GetChild(i).gameObject.GetComponent<TMP_Text>();
+
+            if (i != 0)
+            {
+                TMP_Text TmpTextLeft = HistoryGroup.transform.GetChild(i - 1).gameObject.GetComponent<TMP_Text>();
+                TmpTextRight.SetText(TmpTextLeft.text);
+                TmpTextRight.color = TmpTextLeft.color;
+            }
+            else
+            {
+                TmpTextRight.SetText("");
+                TmpTextRight.color = Color.white;
+            }
+        }
+    }
+    private void ClearEntries()
+    {
+        for (int i = 0; i < HistoryGroup.transform.childCount; i++)
+        {
+            TMP_Text TmpText = HistoryGroup.transform.GetChild(i).gameObject.GetComponent<TMP_Text>();
+            TmpText.SetText("");
+        }
+    }
+    public void AddToHistory(string ActionDesc, Color TextColor)
+    {
+        HistoryGroup.SetActive(true);
+        this.ClearTimer = ClearTime;
+        ShiftEntries();
+        TMP_Text NewText = HistoryGroup.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
+        NewText.SetText(ActionDesc);
+        NewText.color = TextColor;
+    }
+
+    public void DeactivateHistoryGroup()
+    {
+        HistoryGroup.SetActive(false);
+    }
+
+
+}
